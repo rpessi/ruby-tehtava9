34fe87253b44bc39863eb4d6d240a6a4a4675057
Riitta Pessi <riitta@pessi.fi>
2025-03-14 17:06:12 +0200

    Ruby harjoittelutehtävä 9

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,14 @@
-# kirjoita koodi tänne
+# Tee metodi esiintymat, joka saa parametriksi taulukon ja palauttaa hashin, jonka avaimina
+# ovat taulukon alkiot ja arvoina alkioiden esiintymislukumäärät taulukossa
+
+def esiintymat(taulukko)
+  h = {}
+  taulukko.length.times do |i|
+    if not h.has_key?(taulukko[i])
+      h[taulukko[i]] = 0
+    end
+  h[taulukko[i]] = h[taulukko[i]] + 1
+  end
+return h
+end
+
